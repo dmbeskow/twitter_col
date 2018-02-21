2898c33d876d5b592c38043b59f1a53e22cd5e51
David Beskow <dbeskow@GS17928.SP.CS.CMU.EDU>
2018-02-21 09:52:37 -0500

    changed repo name

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='twcol',
+setup(name='twitter_col',
       version='0.1',
       description='Functions to help collect and analyze twitter data',
-      url='http://github.com/dbeskow/twcol',
+      url='http://github.com/dbeskow/twitter_col',
       author='David Beskow',
       author_email='dnbeskow@gmail.com',
       license='MIT',
-      packages=['twcol'],
+      packages=['twitter_col'],
       zip_safe=False)
---
+++ twitter_col/botHunter.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 21 07:51:25 2018
+
+@author: dmbes
+"""
+
