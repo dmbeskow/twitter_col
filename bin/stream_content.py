d3dcccfbab554bc51c23aa49349c5926e31a8c22
David Beskow <dnbeskow@gmail.com>
2018-12-27 23:09:23 -0500

    added stream listener

--- bin/stream_content.py
+++ bin/stream_content.py
@@ -14,45 +14,40 @@ import argparse
 ## authentication
 import tweepy
 
-from pathlib import Path
-home = str(Path.home())
 
-with open(home + '/Dropbox/CMU_cloud/creds/keys.json', 'r') as infile:
-    keys = json.load(infile)
-
-keys = keys['three']
-
-consumer_key = keys['consumer_key']
-consumer_secret = keys['consumer_secret']
-access_key = keys['access_token']
-access_secret = keys['access_secret']
-
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_key, access_secret)
-
-api = tweepy.API(auth, wait_on_rate_limit=True,
-				   wait_on_rate_limit_notify=True)
 
 def main():
-    parser=argparse.ArgumentParser(description="HW5: Yelp Ratings with Logistic Regression and liblinear SVM")
-    parser.add_argument("-train",help="File Path for Training Data" , type=str, required=True)
-    parser.add_argument("-test",help="File Path for Testing Data" , type=str, required=True)
-    parser.add_argument("-model",help="algorithm: 'log' or 'svm'" ,type=str, required = True)
-    parser.add_argument("-features",help="features for model: 'ctf' or 'df' or 'enhanced'" ,type=str, required = True)
+    parser=argparse.ArgumentParser(description="Streaming Twitter API based on content")
+    parser.add_argument("-keys",help="JSON File with Keys" , type=str, required=True)
+    parser.add_argument("-search_terms",help="Text file with search terms (1 per line)" , type=str, required=True)
+    parser.add_argument("-tag",help="Tag that will be prepended to file names" , type=str, required=True)
     args=parser.parse_args()
     print(args)
     
     print('...')
     print('Parsing Training Data...')
+    
+    keys = json.load(args.keys)
+    
+    consumer_key = keys['consumer_key']
+    consumer_secret = keys['consumer_secret']
+    access_key = keys['access_token']
+    access_secret = keys['access_secret']
+    
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_key, access_secret)
+    
+    api = tweepy.API(auth, wait_on_rate_limit=True,
+    				   wait_on_rate_limit_notify=True)
  
-    listen = SListener(api, 'RT_Sputnik')
+    listen = SListener(api, args.tag)
     stream = tweepy.Stream(auth, listen)
 
     print("Streaming started...")
     
     while True:
         try: 
-            stream.filter(track=['@RTUKnews','@RT_America','@RT_Deutsch','@RTenfrancais','@de_sputnik','@RTarabic','@sputnik_ar','@SputnikNewsUS'], async = True, stall_warnings = True)
+            stream.filter(track=['@RTUKnews'], async = True, stall_warnings = True)
         except KeyboardInterrupt:
             break
         
@@ -61,7 +56,6 @@ def main():
             time.sleep(15*60)
             continue
         
-        #stream.disconnect()
 
 if __name__ == '__main__':
     main()
